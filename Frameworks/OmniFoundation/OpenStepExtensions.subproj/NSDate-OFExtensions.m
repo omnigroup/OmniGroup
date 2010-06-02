386180ec1c016538d6318c18970085627d2a273f
Shigeya Suzuki <shigeya@wide.ad.jp>
2010-06-02 15:46:24 +0900

    NSDate-OFExtension: descriptionWithHTTPFormat should be run in en_US locale

--- Frameworks/OmniFoundation/OpenStepExtensions.subproj/NSDate-OFExtensions.m
+++ Frameworks/OmniFoundation/OpenStepExtensions.subproj/NSDate-OFExtensions.m
@@ -22,6 +22,8 @@ RCS_ID("$Id$")
     static NSDateFormatter *dateFormatter = nil;
     if (dateFormatter == nil) {
         dateFormatter = [[NSDateFormatter alloc] init];
+        NSLocale* us_en_locale = [[[NSLocale alloc] initWithLocaleIdentifier:@"en_US"] autorelease];
+        [dateFormatter setLocale:us_en_locale];
         [dateFormatter setTimeZone:[NSTimeZone timeZoneForSecondsFromGMT:0]];
         [dateFormatter setDateFormat:@"EEE, dd MMM yyyy HH:mm:ss 'GMT'"];
     }
