e07d03bb47855c2eb3f16b85e5b91e37d7970721
Timothy J. Wood <tjw@omnigroup.com>
2010-09-10 15:27:29 -0700

    Copyrights and cleanup.

--- Frameworks/OmniUI/iPad/Examples/FieldEditor/Classes/FieldEditorAppDelegate.m
+++ Frameworks/OmniUI/iPad/Examples/FieldEditor/Classes/FieldEditorAppDelegate.m
@@ -1,13 +1,14 @@
+// Copyright 2010 Omni Development, Inc.  All rights reserved.
 //
-//  FieldEditorAppDelegate.m
-//  FieldEditor
-//
-//  Created by Timothy J. Wood on 8/12/10.
-//  Copyright The Omni Group 2010. All rights reserved.
-//
+// This software may only be used and reproduced according to the
+// terms in the file OmniSourceLicense.html, which should be
+// distributed with this project and can also be found at
+// <http://www.omnigroup.com/developer/sourcecode/sourcelicense/>.
 
 #import "FieldEditorAppDelegate.h"
 #import "FieldEditorViewController.h"
+
+RCS_ID("$Id$")
 
 @implementation FieldEditorAppDelegate
 
