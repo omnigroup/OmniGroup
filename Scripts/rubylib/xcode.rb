8b06438d2ef7a30488d3151b2cd1f13c982f2a54
Timothy J. Wood <tjw@omnigroup.com>
2015-07-24 17:58:27 -0700

    Added new script.

---
+++ Scripts/rubylib/xcode.rb
@@ -0,0 +1,45 @@
+#!/usr/bin/ruby
+#
+# Copyright 2015 Omni Development, Inc. All rights reserved.
+#
+# This software may only be used and reproduced according to the
+# terms in the file OmniSourceLicense.html, which should be
+# distributed with this project and can also be found at
+# <http://www.omnigroup.com/developer/sourcecode/sourcelicense/>.
+#
+# $Id$
+
+# Utility functions imported by Xcode::Project and Xcode::Workspace
+
+require 'pathname'
+
+module Xcode
+  
+  # Pathname.realpath requires the whole path to exist and .realdirpath requires the parent directory to exist (only the last path component can be missing).
+  def self.real_relative_path(p)
+    fail "Empty path" if p == ""
+  
+    # This should handle the base case of "/" and "."
+    if File.exist?(p)
+      Pathname.new(p).realpath.to_s
+    else
+      real_relative_path(File.dirname(p)) + "/" + File.basename(p)
+    end
+  end
+  
+  # Support for checking out workspaces and projects in a shared cache under the control of a calling script
+  $CheckoutLocationBlock = nil
+  def self.checkout_location_block=(block)
+    $CheckoutLocationBlock = block
+  end
+  def self.checkout_location(path)
+    if $CheckoutLocationBlock
+      $CheckoutLocationBlock.call(path)
+    else
+      path
+    end
+  end
+  
+  
+end
+
